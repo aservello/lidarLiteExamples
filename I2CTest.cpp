e66860f5a10ab54a3bab47eaf45f76c701519e5c
aservello <90013853+aservello@users.noreply.github.com>
2022-04-21 20:05:57 -0400

    I2C Test

---
+++ I2CTest.cpp
@@ -0,0 +1,42 @@
+/** 
+
+LidarLite I2C Test
+
+*/
+
+#include <Wire.h>
+#include <LIDARLite.h>
+
+// Globals
+LIDARLite lidarLite; // variable to be used to set stuff
+int cal_cnt = 0; // the count variable
+
+void setup()
+{
+  Serial.begin(9600); // Initialize serial connection to display distance readings
+
+  lidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
+  lidarLite.configure(0); // Change this number to try out alternate configurations
+}
+
+void loop()
+{
+  int dist;
+
+  // At the beginning of every 100 readings, take a measurement with receiver bias correction
+  if ( cal_cnt == 0 ) {
+    dist = lidarLite.distance();      // With bias correction
+  } else {
+    dist = lidarLite.distance(false); // Without bias correction
+  }
+
+  // Increment reading counter
+  cal_cnt++;
+  cal_cnt = cal_cnt % 100;
+
+  // Display distance
+  Serial.print(dist);
+  Serial.println(" cm");
+
+  delay(10);
+}
